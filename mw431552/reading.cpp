bb5967d7ad31784d2fe28a85147abd8d8d1a6f0e
Maria Wysogląd <mw431552@students.mimuw.edu.pl>
2025-06-09 00:02:46 +0200

    delta fixing *****

--- mw431552/reading.cpp
+++ mw431552/reading.cpp
@@ -252,7 +252,7 @@ unordered_map<int, long long> delta_stepping_basic(unordered_map<int, Vertex> ve
     long long k = 0;
     bool continue_running = true;
 
-    while (continue_running && k < 40) {
+    while (continue_running) {
         set<int> A = buckets[k];
         bool filled_buckets = 0;
         for (const auto& b : buckets) {
