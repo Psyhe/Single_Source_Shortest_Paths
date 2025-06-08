f43f13f29e0456de33ddc2cff982577fca18983d
Maria Wysogląd <mw431552@students.mimuw.edu.pl>
2025-06-08 13:29:21 +0200

    preventing data race

--- mw431552/reading.cpp
+++ mw431552/reading.cpp
@@ -110,21 +110,30 @@ void relax_edge(
     long long new_d = min(d_v, d_u + w);
 
     if (new_d < d_v) {
-        MPI_Win_lock(MPI_LOCK_EXCLUSIVE, owner_rank, 0, win_d);
-        MPI_Put(&new_d, 1, MPI_LONG_LONG, owner_rank, local_idx, 1, MPI_LONG_LONG, win_d);
-        MPI_Win_flush(owner_rank, win_d);
-        MPI_Win_unlock(owner_rank, win_d);
 
-        long long updated = 1;
-        MPI_Win_lock(MPI_LOCK_EXCLUSIVE, owner_rank, 0, win_changed);
-        MPI_Put(&updated, 1, MPI_LONG_LONG, owner_rank, local_idx, 1, MPI_LONG_LONG, win_changed);
-        MPI_Win_flush(owner_rank, win_changed);
-        MPI_Win_unlock(owner_rank, win_changed);
 
-        if (owner_rank == rank) {
-            local_d[local_idx] = new_d;
-            local_changed[local_idx] = 1;
+        MPI_Win_lock(MPI_LOCK_SHARED, owner_rank, 0, win_d);
+        MPI_Get(&d_v, 1, MPI_LONG_LONG, owner_rank, local_idx, 1, MPI_LONG_LONG, win_d);
+        
+        if (new_d < d_v) {
+            MPI_Win_lock(MPI_LOCK_EXCLUSIVE, owner_rank, 0, win_d);
+            MPI_Put(&new_d, 1, MPI_LONG_LONG, owner_rank, local_idx, 1, MPI_LONG_LONG, win_d);
+            MPI_Win_flush(owner_rank, win_d);
+            MPI_Win_unlock(owner_rank, win_d);
+
+            long long updated = 1;
+            MPI_Win_lock(MPI_LOCK_EXCLUSIVE, owner_rank, 0, win_changed);
+            MPI_Put(&updated, 1, MPI_LONG_LONG, owner_rank, local_idx, 1, MPI_LONG_LONG, win_changed);
+            MPI_Win_flush(owner_rank, win_changed);
+            MPI_Win_unlock(owner_rank, win_changed);
+
+            if (owner_rank == rank) {
+                local_d[local_idx] = new_d;
+                local_changed[local_idx] = 1;
+            }
         }
+
+        MPI_Win_unlock(owner_rank, win_d);
     }
 }
 
@@ -490,7 +499,7 @@ int main(int argc, char** argv) {
     infile.close();
 
     cout << "Processing with IOS" << endl;
-    unordered_map<int, long long> final_values = delta_stepping_IOS(my_vertices, global_root, rank, num_processes, num_vertices);
+    unordered_map<int, long long> final_values = delta_stepping_basic(my_vertices, global_root, rank, num_processes, num_vertices);
 
     // Dummy output for testing (write -1 as shortest path for each vertex)
     std::ofstream outfile(output_file);
