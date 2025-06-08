b01f54c0adb844a5272d91f104fc5b6a052d7f3a
Maria Wysogląd <mw431552@students.mimuw.edu.pl>
2025-06-08 02:56:11 +0200

    experiment

--- mw431552/reading.cpp
+++ mw431552/reading.cpp
@@ -316,7 +316,7 @@ void process_bucket_first_phase_IOS(
     for (int u : A) {
         Vertex& current_vertex = vertex_mapping[u];
         for (Edge& e : current_vertex.edges) {
-            if (e.weight < delta) {
+            // if (e.weight < delta) {
                 relax_edge(u, e, rank, num_vertices, num_procs,
                         vertex_mapping, local_d, local_changed, local_d_prev,
                         win_d, win_changed);
