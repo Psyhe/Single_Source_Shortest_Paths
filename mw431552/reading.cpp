7d69b183bd880f4b15c0e8ff021cecf04bb5769c
Maria Wysogląd <mw431552@students.mimuw.edu.pl>
2025-06-08 23:41:24 +0200

    basic problem

--- mw431552/reading.cpp
+++ mw431552/reading.cpp
@@ -297,7 +297,7 @@ unordered_map<int, long long> delta_stepping_basic(unordered_map<int, Vertex> ve
             }
             cout << endl;
 
-            local_flag = !A.empty();
+            local_flag = !(A.size() == 0);
             MPI_Allreduce(&local_flag, &global_flag, 1, MPI_INT, MPI_LOR, MPI_COMM_WORLD);
         }
 
