e77c72b6b9e4b0ae12725cca55e3f6cd57734fde
Maria Wysogląd <mw431552@students.mimuw.edu.pl>
2025-06-09 13:15:49 +0200

    new tests

--- sbatch_run_tests.sh
+++ sbatch_run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH --job-name sssp-tests
 #SBATCH --output output.txt
-#SBATCH --account "g101-2284"
+#SBATCH --account "g100-2284"
 #SBATCH --nodes 2
-#SBATCH --ntasks-per-node 2
+#SBATCH --ntasks-per-node 48
 #SBATCH --time 00:10:00
 
 module load common/python/3.11
