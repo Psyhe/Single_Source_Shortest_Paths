d188c2a98fd53cfce3c632684d6a505e4bc1e8ef
Maria Wysogląd <mw431552@students.mimuw.edu.pl>
2025-06-09 12:51:05 +0200

    basic

--- sbatch_run_tests.sh
+++ sbatch_run_tests.sh
@@ -3,7 +3,7 @@
 #SBATCH --output output.txt
 #SBATCH --account "g101-2284"
 #SBATCH --nodes 2
-#SBATCH --ntasks-per-node 2
+#SBATCH --ntasks-per-node 48
 #SBATCH --time 00:10:00
 
 module load common/python/3.11
