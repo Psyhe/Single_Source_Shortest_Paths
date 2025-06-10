863e9d21ee49768a50deb8df61ab27cd1254c6d8
Maria Wysogląd <mw431552@students.mimuw.edu.pl>
2025-06-10 04:17:50 +0200

    25 again

--- sbatch_run_tests.sh
+++ sbatch_run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
-#SBATCH --job-name 80_big_test_delta
-#SBATCH --output output_delta_80.txt
+#SBATCH --job-name 25_again_big_test_delta
+#SBATCH --output output_delta_25_again.txt
 #SBATCH --account "g101-2284"
 #SBATCH --nodes 4
 #SBATCH --ntasks-per-node 24
